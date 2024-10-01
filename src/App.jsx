da666b6e659f4e8f9227609386be8df50654ebff
Aswathi S <aswathis@presidio.com>
2024-10-01 19:53:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'pale-yellow':"#FEFAEE",
+      'neon-yellow':"#FEED5A",
+      'bright-yellow':"#FEF1BB",
+      'black':'#000',
+      'white':'#fff',
+      'white-gray':"#F9F8F7",
+    },
+    extend: {},    
+  },
+  plugins: [],
+}
